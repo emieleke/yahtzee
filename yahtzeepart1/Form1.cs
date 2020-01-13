eb4bb2fb129ea31d7bb59f6ecf89ef1e587c5288
emieleke <0321551@student.rocvantwente.nl>
2020-01-13 11:15:48 +0100

    test

--- yahtzeepart1/Form1.cs
+++ yahtzeepart1/Form1.cs
@@ -16,5 +16,10 @@ namespace yahtzeepart1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("hello");
+        }
     }
 }
